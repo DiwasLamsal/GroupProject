4cb99e98362299d0e8da5ae3d3ce12ddd2941275
Diwas Lamsal <diwaslamsal123@hotmail.com>
2019-03-18 18:43:19 +0545

    Icons to the admin navigation

--- app/views/administrators/AdminNavigation.php
+++ app/views/administrators/AdminNavigation.php
@@ -1,31 +1,71 @@
 <li>
-  <a href = "/GroupProject/public/AdminHome"><div style="width: 100%; height: 100%;">Dashboard</div></a>
+  <a href = "/GroupProject/public/AdminHome">
+    <div style="width: 100%; height: 100%;">
+      <img src = "/GroupProject/public/resources/images/dashboardicon.png">
+      <span class = "navText">&nbsp; Dashboard</span>
+    </div>
+  </a>
 </li>
 
 <li>
-  <a href = "/GroupProject/public/ManageAdministrators"><div style="width: 100%; height: 100%;">Administrators</div></a>
+  <a href = "/GroupProject/public/ManageAdministrators">
+    <div style="width: 100%; height: 100%;">
+      <img src = "/GroupProject/public/resources/images/administratoricon.png">
+      <span class = "navText">&nbsp; Administrators</span>
+    </div>
+  </a>
 </li>
 
 <li>
-  <a href = "/GroupProject/public/ManageModuleLeaders"><div style="width: 100%; height: 100%;">Module Leaders</div></a>
+  <a href = "/GroupProject/public/ManageModuleLeaders">
+    <div style="width: 100%; height: 100%;">
+      <img src = "/GroupProject/public/resources/images/lecturericon.png">
+      <span class = "navText">&nbsp; Module Leaders</span>
+    </div>
+  </a>
 </li>
 
 <li>
-  <a href = "/GroupProject/public/ManageStudents"><div style="width: 100%; height: 100%;">Students</div></a>
+  <a href = "/GroupProject/public/ManageStudents">
+    <div style="width: 100%; height: 100%;">
+      <img src = "/GroupProject/public/resources/images/studenticon.png">
+      <span class = "navText">&nbsp; Students</span>
+    </div>
+  </a>
 </li>
 
 <li>
-  <a href = "/GroupProject/public/ManageCourses"><div style="width: 100%; height: 100%;">Courses</div></a>
+  <a href = "/GroupProject/public/ManageCourses">
+    <div style="width: 100%; height: 100%;">
+      <img src = "/GroupProject/public/resources/images/course.png">
+      <span class = "navText">&nbsp; Courses</span>
+    </div>
+  </a>
 </li>
 
 <li>
-  <a href = "/GroupProject/public/ManageModules"><div style="width: 100%; height: 100%;">Modules</div></a>
+  <a href = "/GroupProject/public/ManageModules">
+    <div style="width: 100%; height: 100%;">
+      <img src = "/GroupProject/public/resources/images/module.png">
+      <span class = "navText">&nbsp; Modules</span>
+    </div>
+  </a>
 </li>
 
 <li>
-  <a href = "/GroupProject/public/ManageAnnouncements"><div style="width: 100%; height: 100%;">Announcements</div></a>
+  <a href = "/GroupProject/public/ManageAnnouncements">
+    <div style="width: 100%; height: 100%;">
+      <img src = "/GroupProject/public/resources/images/addannouncement.png">
+      <span class = "navText">&nbsp; Announcements</span>
+    </div>
+  </a>
 </li>
 
 <li>
-  <a href = "/GroupProject/public/ManageLevels"><div style="width: 100%; height: 100%;">Levels</div></a>
+  <a href = "/GroupProject/public/ManageLevels">
+    <div style="width: 100%; height: 100%;">
+      <img src = "/GroupProject/public/resources/images/levelicon.png">
+      <span class = "navText">&nbsp; Levels</span>
+    </div>
+  </a>
 </li>
