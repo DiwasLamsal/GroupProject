de3314d58b9264a6e1f193edf3d3e185cdb63e4e
Diwas Lamsal <diwaslamsal123@hotmail.com>
2019-04-21 16:41:42 +0545

    PAT

--- app/controllers/ModuleLeaderPAT.php
+++ app/controllers/ModuleLeaderPAT.php
@@ -3,6 +3,13 @@
   class ModuleLeaderPAT extends Controller{
 
     public function index(){
+      $studentClass = new DatabaseTable('students');
+
+      if (session_status() == PHP_SESSION_NONE) {
+        session_start();
+      }
+      $modules = $moduleClass->find('mluid', $_SESSION['loggedin']['uid']);
+
 
       $template = '../app/views/moduleLeaders/viewPAT.php';
       $content = loadTemplate($template, []);
---
+++ app/views/moduleLeaders/viewPAT.php
@@ -0,0 +1,20 @@
+<ul class="breadcrumb">
+  <li><a href="/GroupProject/public/ModuleLeaderHome">
+    <img src = "/GroupProject/public/resources/images/house.png">&nbsp; Dashboard</a>
+  </li>
+
+  <li>PAT</li>
+</ul>
+
+
+<div class = "adminManageTable">
+  <div class = "tableTitle">
+    <h1 class = "tableHeading">Students for whom you are PAT</h1>
+  </div>
+
+  <div class = "content" style="text-align: left; margin: 15px; line-height: 1.6;">
+    
+
+
+  </div>
+</div>
