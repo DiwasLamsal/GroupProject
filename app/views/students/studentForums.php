77a711e1e86b4b252bfac5297ff6dcf5b2aac7c6
Diwas Lamsal <diwaslamsal123@hotmail.com>
2019-04-25 09:57:46 +0545

    Forum Guidelines

--- app/views/students/studentForums.php
+++ app/views/students/studentForums.php
@@ -1,3 +1,19 @@
-<h1>
-  Student Forums
-</h1>
+<div class = "adminManageTable">
+
+  <div class = "tableTitle">
+    <h1 class = "tableHeading">Forum Guidelines</h1>
+  </div>
+  <div class = "content" style="text-align: left; margin: 30px; line-height: 1.6; margin-bottom: 5px; margin-right: 0;">
+    <ul class = "forumGuideline">
+      <li>Do not post Spam/Advertising/Promotion content</li>
+      <li>Do not post any material that could be offensive</li>
+      <li>Do refrain from posting same question multiple times</li>
+      <li>Be descriptive about your questions</li>
+      <li>Do not argue unnecessarily among each other</li>
+      <li>Do not post any vulgar content</li>
+      <li>Do not post your own assignments</li>
+    </ul><br>
+    <b>Note*:</b> Failure to abide by these conditions could lead to serious actions depending on the
+    extremeness of situation.
+  </div>
+</div>
